e4d3b890017348e90fbfe13a57ba0b5c0d5808f1
Zach Davis <zachdavis0099@gmail.com>
2022-07-17 09:59:43 -0500

    Link API

--- ByteCoin/Controller/ViewController.swift
+++ ByteCoin/Controller/ViewController.swift
@@ -1,20 +1,62 @@
-//
-//  ViewController.swift
-//  ByteCoin
-//
-//  Created by Angela Yu on 11/09/2019.
-//  Copyright © 2019 The App Brewery. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var currencyLabel: UILabel!
+    @IBOutlet weak var rateLabel: UILabel!
+    @IBOutlet weak var currencyPicker: UIPickerView!
+    
+    var coinManager = CoinManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        currencyPicker.dataSource = self
+        currencyPicker.delegate = self
+        coinManager.delegate = self
+    }
+    
+}
+
+//MARK: - UIPicker Delegate & Data Source
+
+extension ViewController: UIPickerViewDataSource, UIPickerViewDelegate {
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
     }
 
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return coinManager.currencyArray.count
+    }
 
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        coinManager.currencyArray[row]
+    }
+
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        let selectedCurrency = coinManager.currencyArray[row]
+        coinManager.getCoinPrice(selectedCurrency)
+    }
+}
+
+
+
+//MARK: - Coin Manager Delegate
+
+extension ViewController: CoinManagerDelegate {
+    
+    func didUpdateCoin(_ coin: CoinModel) {
+        
+        DispatchQueue.main.sync {
+            rateLabel.text = String(format: "%.5f", coin.rate)
+        }
+    }
+    
+    func didGetError(_ error: Error) {
+        print(error)
+    }
+    
+    
 }
 
--- ByteCoin/Model/CoinManager.swift
+++ ByteCoin/Model/CoinManager.swift
@@ -1,19 +1,71 @@
-//
-//  CoinManager.swift
-//  ByteCoin
-//
-//  Created by Angela Yu on 11/09/2019.
-//  Copyright © 2019 The App Brewery. All rights reserved.
-//
 
 import Foundation
 
+protocol CoinManagerDelegate {
+    func didUpdateCoin(_ coin: CoinModel)
+    func didGetError(_ error: Error)
+}
+
 struct CoinManager {
-    
-    let baseURL = "https://rest.coinapi.io/v1/exchangerate/BTC"
-    let apiKey = "YOUR_API_KEY_HERE"
+
     
     let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
+    let baseURL = "https://rest.coinapi.io/v1/exchangerate"
+    let apiKey = "?apikey=A9D6BE9E-B84F-4920-ACAA-7B7C7EC76845"
+    
+    
+    var delegate: CoinManagerDelegate?
+    
+    
+    func getCoinPrice(_ currency: String) {
+        
+        let urlString = "\(baseURL)/\(currency)/USD\(apiKey)"
+        
+        // 1. create url
+        
+        if let url = URL(string: urlString) {
+            
+            // 2. Create url Session
+            
+            let session = URLSession(configuration: .default)
+            
+            // 3. Create task
+            
+            let task = session.dataTask(with: url, completionHandler: { data, response, error in
+                if error != nil {
+                    print(error!)
+                    return
+                }
+                if let safeData = data {
+
+                    if let coin = self.parseJSON(safeData) {
+                        delegate?.didUpdateCoin(coin)
+                    }
+                }
+            })
+            
+            // 4. start task
+            
+            task.resume()
+        }
+    }
+    
+    func parseJSON(_ coinData: Data) -> CoinModel? {
+        let decoder = JSONDecoder()
+        do {
+            let decodedData = try decoder.decode(CoinData.self, from: coinData)
+            let rate = decodedData.rate
+            
+            let coin = CoinModel(rate: rate)
+            return coin
+
+        } catch {
+            delegate?.didGetError(error)
+            return nil
+        }
+    }
+    
+    
 
     
 }
